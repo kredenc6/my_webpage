8f4280a76360bae76ee58fb681d485d7fd006d43
kredenc6 <weeve@email.cz>
2019-10-10 20:07:15 +0200

    background pics modified, gForm animation, project boxes redesigned

--- mainDev.js
+++ mainDev.js
@@ -15,17 +15,25 @@ function showMobileMenu() {
   mobileMenuNode.style.display = "block";
   closeIconNode.style.display = "inline";
   menuIconNode.style.display = "none";
-  menuIconContainerNode.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  menuIconContainerNode.style.background = "none";
 }
 
 function hideMobileMenu() {
   mobileMenuNode.style.display = "none";
   closeIconNode.style.display = "none";
   menuIconNode.style.display = "inline";
-  menuIconContainerNode.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+  menuIconContainerNode.style.background = "linear-gradient(45deg, rgba(0, 0, 0, 0) 65%, rgba(0, 0, 0, 0.1))";
 }
 
 // ***** GOOGLE FORM LOADING *****
 function hideLoadingMessage() {
+  const iframeNode = document.querySelector("iframe");
   document.getElementById("loading_message").style.display = "none";
+  iframeNode.style.opacity = "0.7";
+  iframeNode.addEventListener("mouseover", ()=> {
+    event.target.style.opacity = "1";
+  });
+  iframeNode.addEventListener("mouseout", ()=> {
+    event.target.style.opacity = "0.7";
+  });
 }
